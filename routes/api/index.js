cb131cf5328c8eae9dc3724ae39c0c5fcd19be29
Andrew <echowhite93@yahoo.com>
2022-09-08 07:28:00 -0700

    created routes to find users

---
+++ routes/api/index.js
@@ -0,0 +1,14 @@
+const { allUsers, singleUser } = require('../../controllers/userController');
+
+const router = require('express').Router();
+
+router.route('/users').get(allUsers).post(newUser);
+router.route('/users/:id').get(singleUser).put(updateUser).delete(deleteUser);
+
+router.route('/users/:userId/friends/:friendId').post(newFriend).delete(deleteFriend);
+
+router.route('/thoughts').get(allThoughts).post(newThoughts);
+router.route('/thoughts/:id').get(singleThought).put(updateThought).delete(deleteThought);
+
+router.route('/thoughts/:thoughtId/reactions/:reactionId?').post(newReaction).delete(deleteReaction);
+module.exports = router;
