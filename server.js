34e6668035433885707b967a7f8260a7c20843c8
Andrew <echowhite93@yahoo.com>
2022-09-08 07:29:46 -0700

    added routes to server

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const routes = require('./routes');
 const app = express();
 
 const PORT = process.env.PORT || 3001;
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
