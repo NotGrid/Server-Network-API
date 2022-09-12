21c80a9211f8a4bb930cd11325d1ed2dcd1eb3d3
Andrew <echowhite93@yahoo.com>
2022-09-12 16:33:05 -0700

    completed user functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,41 @@
-const { User, Thoughts } = require('../models');
+const { User, Thoughts } = require('../models/User');
 
 const userController = {
     allUsers(req, res) {
         User.find().then(dbUsers => {
-            res.json(dbUser);
+            res.json(dbUsers);
         });
     },
     singleUser(req, res) {
         User.findById(req.params.id).then(dbUsers => {
-            res.json(dbUser);
+            res.json(dbUsers);
         });
     },
-}
+    newUser(req, res) {
+        User.create(req.body).then(dbUsers => {
+            res.json(dbUsers);
+        });
+    },
+    updateUser(req, res) {
+        User.findOneAndUpdate(req.params.id).then(dbUsers => {
+            res.json(dbUsers);
+        });
+    },
+    deleteUser(req, res) {
+        User.findOneAndDelete(req.params.id).then(dbUsers => {
+            res.json(dbUsers);
+        });
+    },
+    newFriend(req, res) {
+        User.create(req.params.id).then(dbUsers => {
+            res.json(dbUsers);
+        });
+    },
+    deleteFriend(req, res) {
+        User.findOneAndDelete(req.params.id).then(dbUsers => {
+            res.json(dbUsers);
+        });
+    },
+};
 
 module.exports = userController;
