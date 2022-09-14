8f70a89b9a4d8b5f1f82cdecc9644b20afc4673d
Andrew <echowhite93@yahoo.com>
2022-09-14 16:48:40 -0700

    reformated to show properly in postman

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,14 @@
-const { User, Thoughts } = require('../models/User');
+const { User, Thoughts } = require('../models');
 
 const userController = {
     allUsers(req, res) {
         User.find().then(dbUsers => {
+            console.log(dbUsers);
             res.json(dbUsers);
-        });
+        }).catch(error => {
+            console.log(error);
+            res.json(error.message);
+        })
     },
     singleUser(req, res) {
         User.findById(req.params.id).then(dbUsers => {
@@ -17,22 +21,22 @@ const userController = {
         });
     },
     updateUser(req, res) {
-        User.findOneAndUpdate(req.params.id).then(dbUsers => {
+        User.findOneAndUpdate(req.params.id, req.body, {new: true}).then(dbUsers => {
             res.json(dbUsers);
         });
     },
     deleteUser(req, res) {
-        User.findOneAndDelete(req.params.id).then(dbUsers => {
+        User.findOneAndDelete(req.params.id, {new: true}).then(dbUsers => {
             res.json(dbUsers);
         });
     },
     newFriend(req, res) {
-        User.create(req.params.id).then(dbUsers => {
+        User.findOneAndUpdate(req.params.userId, {$addToSet: {friends: req.params.friendId}}, {new: true}).then(dbUsers => {
             res.json(dbUsers);
         });
     },
     deleteFriend(req, res) {
-        User.findOneAndDelete(req.params.id).then(dbUsers => {
+        User.findOneAndUpdate(req.params.userId, {$pull: {friends: req.params.friendId}}, {new: true}).then(dbUsers => {
             res.json(dbUsers);
         });
     },
